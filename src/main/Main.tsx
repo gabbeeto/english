641bc391da0538d1638510877baa1b0dcd0475b3
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2024-03-22 10:24:55 -0300

    improve system a little bit

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -3,15 +3,14 @@ import About from './About'
 import Why from './Why'
 import HomeWork from './homework/Main'
 
-
+const hashExists = window.location.hash != ''
 
 export default function Header(): ReactNode {
 let styleForContainer: string = "bg-gradient-to-tr from-cyan-950 to-emerald-950 text-white text-center rounded-2xl p-2 containerExtras "
 return ( <main className="flex flex-col gap-2 items-center justify-center bg-cyan-900">
 <About containerStyle={styleForContainer} / >
 <Why containerStyle={styleForContainer} / >
-<HomeWork containerStyle={styleForContainer} / >
-
+{hashExists && <HomeWork containerStyle={styleForContainer} / >}
 </main>
 
 )
--- src/main/homework/Main.tsx
+++ src/main/homework/Main.tsx
@@ -1,17 +1,18 @@
 import {ReactNode} from "react";
 import HashFnder from "./homeworkObjectForDisplay.js"
 import A1HomeWork from "./a1/Main"
-
+const firstLetter = window.location.hash[1]
+const hashStartsWithA = firstLetter == 'a'
 
 export default function HomeWork({containerStyle}: {containerStyle: string}): ReactNode {
 let summaryOpen: Array[boolean] = [false, false, false, false]
 if(window.location.hash == "#why"){
 summaryOpen = [true, true, true, true]
 }
+
 const hashes = HashFnder()
-console.log(hashes)
-return ( <section id="why" className={`${hashes.current} ${containerStyle}`}><h2 className="textSize-2">Home Work</h2>
-<A1HomeWork hashObj={hashes.a.a1} / >
+return ( <section id="homework" className={`${hashes.current} ${containerStyle}`}><h2 className="textSize-2">Home Work</h2>
+{ hashStartsWithA && <A1HomeWork hashObj={hashes.a.a1} / > }
 
 
 
